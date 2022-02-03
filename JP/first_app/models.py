9385489a9ed269a592507404cabc7ef9bc48bf25
SumanBanikSB <suman33banik@gmail.com>
2022-02-03 23:26:48 +0530

    first commit

---
+++ JP/first_app/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+# Company Model..
+class Company(models.Model):
+    user = models.OneToOneField(User, null = True, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, null=True)
+    position = models.CharField(max_length=200, null = True)
+    description = models.CharField(max_length=2000, null=False)
+    salary = models.IntegerField(null = True)
+    experience = models.IntegerField(null = True)
+    location = models.CharField(max_length=2000, null = True)
+
+    def __str__(self):
+        return self.name
+
+class Candidates(models.Model):
+    category = (
+        ('M', 'male'),
+        ('F','female'),
+        ('Other','Other'),
+    )
+    name = models.CharField(max_length=200, null = True)
+    dob = models.DateField(null = True)
+    gender = models.CharField(max_length = 10, choices=category)
+    mobile = models.CharField(max_length=20)
+    email = models.EmailField(max_length=50)
+    resume = models.FileField()
+    company = models.ManyToManyField(Company, blank = True)
+
+    def __str__(self):
+        return self.name
+
+
